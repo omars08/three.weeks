42049101e423e896cbca9d3ea8326f1e32fdf55f
Kjartan Konráðsson <kjartank17@ru.is>
2017-12-01 16:26:28 +0000

    kjartan

--- Superhero.kj/include/Heroes.h
+++ Superhero.kj/include/Heroes.h
@@ -7,23 +7,23 @@ using namespace std;
 class Heroes
 {
     public:
-        Heroes(){};
-        Heroes(string name, int age, char power);
-        friend istream& operator >> (istream& in, Heroes& Hero);
+        Heroes();                                                 /// Takes in empty strings
+        Heroes(char name[], int age, char power);                 /// Takes in name, age and a char letter
+        friend istream& operator >> (istream& in, Heroes& Hero);  /// Overloading operators
         friend ostream& operator << (ostream& out, Heroes& Hero);
-        void fixdis();
-        void display();
-        void put_shit_heroe_in_file();
-        void get_wannabe_hero();
+        void put_hero_in_file();                                   ///a function which saves the hero
+        void get_wannabe_hero();                                  /// a function which displays all the heroes
+        void binary_file_input();                                 /// function which uses a binary file
 
     private:
-    string _name;
+    char _name[30];                                                /// Declaring arguments
     int _age;
     char _power_letter;
     string power;
-    ofstream fout;
+    ofstream fout;                                                 ///Declaring streams for text files
     ifstream fif;
     string line;
+    int number;
 
 };
 
