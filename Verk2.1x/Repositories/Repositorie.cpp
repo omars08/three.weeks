d43d113faa08c802cfffdf8e0f49705731725be8
Kjartan Konráðsson <kjartank17@ru.is>
2017-12-05 18:38:39 +0000

    kjartan

--- Verk2.1x/Repositories/Repositorie.cpp
+++ Verk2.1x/Repositories/Repositorie.cpp
@@ -1,12 +1,47 @@
 #include "Repositorie.h"
 #include <iostream>
+#include <istream>
 using namespace std;
 Repositorie::Repositorie()
 {
-    //ctor
+
+}
+Repositorie::Repositorie(string _name, int _SSN, int _salary,int _month,int _year)
+{
+    name = _name;
+    SSN = _SSN;
+    salary = _salary;
+    month = _month;
+    year = _year;
 }
 
-Repositorie::~Repositorie()
+ istream& operator >> (istream& in, Repositorie& re)
 {
-    //dtor
+    cout << "Name: ";
+    in >> re.name;
+    cout << "SSN: ";
+    in >> re.SSN;
+    cout << "Salary: ";
+    in >> re.salary;
+    cout << "Month: ";
+    in >> re.month;
+    cout << "Year: ";
+    in >> re.year;
+
+    return in;
+}
+ ostream& operator << (ostream& out, Repositorie& re)
+{
+
+    out << "Name: " << re.name << endl;
+    out << "SSN: " << re.SSN << endl;
+    out << "Salary: " << re.salary << endl;
+    out << "Month: " << re.month << endl;
+    out << "Year: " << re.year << endl;
+
+    return out;
+}
+void Rep_func()
+{
+
 }
--- Verk2.1x/main.cpp
+++ Verk2.1x/main.cpp
@@ -2,12 +2,16 @@
 #include <string>
 #include <fstream>
 #include <UI.h>
+#include <Repositorie.h>
+#include <istream>
 using namespace std;
 
 int main()
 {
-
     UI user_interface;
-    user_interface.test();
+    Repositorie re;
+    cin >> re;
+
+    user_interface.Interface();
     return 0;
 }
