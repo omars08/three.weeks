7819b55421b707a129f7dd82bc990395c502d332
Sindri Dan Garðarsson <sindridan@gmail.com>
2017-12-05 13:36:11 +0000

    sindri

---
+++ FinalProject2/New/include/NewPizzaToppings.h
@@ -0,0 +1,16 @@
+#ifndef NEWPIZZATOPPINGS_H
+#define NEWPIZZATOPPINGS_H
+
+
+class NewPizzaToppings
+{
+    public:
+        NewPizzaToppings();
+        virtual ~NewPizzaToppings();
+        void PrintAvailableToppings();
+
+
+    private:
+};
+
+#endif // NEWPIZZATOPPINGS_H
---
+++ FinalProject2/New/src/NewPizzaToppings.cpp
@@ -0,0 +1,17 @@
+#include "NewPizzaToppings.h"
+#include "StaffLoginUI.h"
+
+
+NewPizzaToppings::NewPizzaToppings()
+{
+    //ctor
+}
+
+NewPizzaToppings::~NewPizzaToppings()
+{
+    //dtor
+}
+void NewPizzaToppings::PrintAvailableToppings()
+{
+
+}
