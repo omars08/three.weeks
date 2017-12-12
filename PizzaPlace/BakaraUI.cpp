ab140564743f44a47f77473aa6385eb5cb6858c7
Kjartan Konráðsson <kjartank17@ru.is>
2017-12-12 13:35:31 +0000

    kjartan

---
+++ PizzaPlace/BakaraUI.cpp
@@ -0,0 +1,11 @@
+#include "BakaraUI.h"
+
+BakaraUI::BakaraUI()
+{
+    //ctor
+}
+
+BakaraUI::~BakaraUI()
+{
+    //dtor
+}
---
+++ PizzaPlace/BakaraUI.h
@@ -0,0 +1,16 @@
+#ifndef BAKARAUI_H
+#define BAKARAUI_H
+
+
+class BakaraUI
+{
+    public:
+        BakaraUI();
+        virtual ~BakaraUI();
+
+    protected:
+
+    private:
+};
+
+#endif // BAKARAUI_H
